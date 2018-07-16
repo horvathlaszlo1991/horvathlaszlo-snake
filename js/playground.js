10e6e66d4583d43eca4e0eaef6ff0788084e53a5
Laszlo Horvath <horvathlaszlo1991@gmail.com>
2018-07-16 13:03:44 +0200

    created js files

---
+++ js/playground.js
@@ -0,0 +1,13 @@
+class Playground {
+  constructor(width, targetDiv) {
+    this.width = width;
+    this.targetDiv = targetDiv;
+    this.generate();
+  }
+
+  generate() {
+    for (var i = 0; i < (this.width * this.width); i++) {
+
+    }
+  }
+}
